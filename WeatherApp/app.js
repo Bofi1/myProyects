0e99952a07dc93bbeb7271511fe4c23105797bde
Bofi1 <angelborque03github@gmail.com>
2025-12-04 10:52:14 -0700

    Weather App done

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -9,10 +9,7 @@ searchBar.addEventListener("keydown", (event) => {
 let url = "http://api.openweathermap.org/data/2.5/weather?q=";
 let key = "b3a06a3c64c82b438ac9b0b5c93fa703";
 
-let world = document.getElementById("world");
-
 function getWeatherApi() {
-  world.classList.add("hidden");
   fetch(`${url}${searchBar.value}&appid=${key}&units=metric`)
     .then((res) => res.json())
     .then((data) => sendToDom(data));
@@ -39,16 +36,24 @@ let temp = document.getElementById("temp");
 let desc = document.getElementById("description");
 let hum = document.getElementById("humidity");
 let main = document.getElementById("main");
-
-// searchBar.value = "New York";
-// getWeatherApi();
+let world = document.getElementById("world");
 
 function sendToDom(data) {
-  city.innerText = `${data.name}, ${data.sys.country}`;
-  showActualDate();
-  temp.innerText = `${Math.round(data.main.temp)}°c`;
-  desc.innerText = data.weather[0].description;
-  hum.innerText = `humidity: ${data.main.humidity}%`;
+  if (data.cod == 200) {
+    world.classList.add("hidden");
+    searchBar.classList.remove("errorBorder");
+
+    city.innerText = `${data.name}, ${data.sys.country}`;
+    showActualDate();
+    temp.innerText = `${Math.round(data.main.temp)}°c`;
+    desc.innerText = data.weather[0].description;
+    hum.innerText = `humidity: ${data.main.humidity}%`;
+  } else {
+    searchBar.classList.add("animation-error");
+    searchBar.classList.add("errorBorder");
+
+    setTimeout(() => searchBar.classList.remove("animation-error"), 101);
+  }
 }
 
 function showActualDate() {
@@ -59,3 +64,7 @@ function showActualDate() {
 
   todayDate.innerText = actualDate;
 }
+
+searchBar.addEventListener("keydown", () => {
+  searchBar.classList.remove("errorBorder");
+});
